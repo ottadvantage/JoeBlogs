c1c81c355b576ec705e1b15395a26352c1dacd78
Michael J. Gibbs <mjg@overthetop.com>
2015-12-09 15:41:03 -0800

    Modified to reflect new format that Wordpress is using in it's XMLRPC interface.

--- JoeBlogs/xmlRpcStructs/TagInfo.cs
+++ JoeBlogs/xmlRpcStructs/TagInfo.cs
@@ -12,7 +12,7 @@ namespace JoeBlogs
         /// <summary>
         /// The id.
         /// </summary>
-        public string tag_id;
+        public int tag_id;
 
         /// <summary>
         /// The name. This is also usually the textual representation of the tag.
